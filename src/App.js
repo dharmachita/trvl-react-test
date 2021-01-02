66aefa014fe6594e77644dc6812fa95987aea0f8
Mauro Rambo <mauroemmanuel22@gmail.com>
2021-01-02 13:10:25 -0300

    footer y home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles.css";
 import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
@@ -9,8 +11,9 @@ export default function App() {
       <Router>
         <NavBar />
         <Switch>
-          <Route exact path="/" />
+          <Route exact path="/" component={Home} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import Cards from "../Cards";
+//import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      {
+        <div className="home">Hola soy el conenido del Home</div>
+        //<HeroSection />
+        //<Cards />
+      }
+    </>
+  );
+}
+
+export default Home;
